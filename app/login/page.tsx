f20ac56b251d70af6ec94db8f10c004b3a1eac04
Andrew William Staines <andrewgithub007@gmail.com>
2025-04-27 11:15:22 +0530

    Modified the redirection page to home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
         <h2>Login</h2>
         <input type="text" placeholder="Username" className={styles.input} />
         <input type="password" placeholder="Password" className={styles.input} />
-        <Link href="/login" className={styles.button}>Login</Link>
+        <Link href="/home" className={styles.button}>Login</Link>
       </div>
     </div>
   );
