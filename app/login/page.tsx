4ccd7f6b54ab7bfd49a11a32c29d7d382c87d8d3
Andrew William Staines <andrewgithub007@gmail.com>
2025-04-27 11:10:25 +0530

    Added a simple login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from './login.module.css';
+import Link from 'next/link';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className={styles.loginPage}>
+      <div className={styles.loginPop}>
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" className={styles.input} />
+        <input type="password" placeholder="Password" className={styles.input} />
+        <Link href="/login" className={styles.button}>Login</Link>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
